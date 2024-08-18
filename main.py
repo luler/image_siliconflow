e6c8c305e1febe2085fd84fa46d3c2ba6fb2be5b
我只想看看蓝天 <1207032539@qq.com>
2024-08-18 22:14:23 +0800

    优化

--- main.py
+++ main.py
@@ -145,4 +145,4 @@ if __name__ == "__main__":
             submit_btn_2.click(dosomething2, inputs=[input_2_1, input_2_2, input_2_3, ],
                                outputs=output_image_2)
 
-    demo.launch(server_port=7861)
+    demo.launch(server_name='0.0.0.0', server_port=7861)
