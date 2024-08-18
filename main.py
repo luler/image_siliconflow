8aee515c04c5158355cbd42980640da677184afa
我只想看看蓝天 <1207032539@qq.com>
2024-08-18 21:44:51 +0800

    增加flux模型

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ import retrying
 from PIL import Image
 
 
+@retrying.retry(stop_max_attempt_number=2, wait_fixed=1000)
+def translate(text):
+    with requests.get('https://lingva.thedaviddelta.com/api/v1/auto/en/' + text) as response:
+        text = ''
+        if response.status_code == 200:
+            data = response.json()
+            text = data['translation']
+        return text
+
+
 # 图生图
 @retrying.retry(stop_max_attempt_number=2, wait_fixed=1000)
 def sdxl_img2img(image, inference_steps, guidance_scale, image_size):
@@ -52,22 +62,86 @@ def sdxl_img2img(image, inference_steps, guidance_scale, image_size):
             return img
 
 
-def dosomething(image, inference_steps, guidance_scale, image_size):
+# 图生图
+@retrying.retry(stop_max_attempt_number=2, wait_fixed=1000)
+def flux_schnell(prompt, inference_steps, image_size):
+    # 获取图像的宽度和高度
+    image_size = {
+        '1:1': '1024x1024',
+        '1:2': '512x1024',
+        '3:2': '768x512',
+        '3:4': '768x1024',
+        '16:9': '1024x576',
+        '9:16': '576x1024',
+    }[image_size]
+
+    # 设置请求头
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        'Authorization': 'Bearer ' + os.getenv('SILICONFLOW_API_KEY'),
+    }
+    # 设置请求参数
+    data = {
+        "prompt": prompt,
+        "image_size": image_size,
+        "num_inference_steps": inference_steps,
+    }
+
+    url = "https://api.siliconflow.cn/v1/black-forest-labs/FLUX.1-schnell/text-to-image"
+    with requests.post(url, data=json.dumps(data), headers=headers) as response:
+        res = response.json()
+        image_url = res['images'][0]['url']
+        with requests.get(image_url) as image_response:
+            img = Image.open(io.BytesIO(image_response.content))
+            return img
+
+
+def dosomething1(image, inference_steps, guidance_scale, image_size):
     return sdxl_img2img(image, inference_steps, guidance_scale, image_size)
+
+
+def dosomething2(prompt, inference_steps, image_size):
+    return flux_schnell(translate(prompt), inference_steps, image_size)
 
 
 # 这里是主程序的代码
 if __name__ == "__main__":
     # 加载配置
     dotenv.load_dotenv()
-    iis = [
-        gr.Image(label='选择原图', type='pil'),
-        gr.Number(minimum=1, maximum=50, value=20),
-        gr.Slider(minimum=0, maximum=20, value=7.5, step=0.1),
-        gr.Dropdown(['1:1', '1:2', '3:2', '3:4', '16:9', '9:16', ], label='生成图片比例', value='1:1')
-    ]
-    oo = gr.Image(label='生成的图片')
 
-    demo = gr.Interface(dosomething, inputs=iis, outputs=oo, title='图片动漫化（基于Stable Diffusion模型）')
+    # 定义每个选项卡中的内容
+    with gr.Blocks() as demo:
+        with gr.Tab("图片动漫化"):
+            gr.Markdown(
+                "<h2 style='text-align: center;'>图片动漫化（基于Stable Diffusion模型）</h2>")  # Title for Tab 1
+            with gr.Row():
+                with gr.Column():
+                    input_1_1 = gr.Image(label='选择原图', type='pil')
+                    input_1_2 = gr.Number(minimum=1, maximum=50, value=20)
+                    input_1_3 = gr.Slider(minimum=0, maximum=20, value=7.5, step=0.1)
+                    input_1_4 = gr.Dropdown(['1:1', '1:2', '3:2', '3:4', '16:9', '9:16', ], label='生成图片比例',
+                                            value='1:1')
+                    submit_btn_1 = gr.Button("提交", variant="primary")
+                with gr.Column():
+                    output_image_1 = gr.Image(label="生成的图片")
 
-    demo.launch(server_name='0.0.0.0', server_port=7861)
+            submit_btn_1.click(dosomething1, inputs=[input_1_1, input_1_2, input_1_3, input_1_4, ],
+                               outputs=output_image_1)
+        with gr.Tab("FLUX.1-schnell(trial)"):
+            gr.Markdown("<h2 style='text-align: center;'>图片生成（基于FLUX.1-schnell模型）</h2>")  # Title for Tab 1
+            with gr.Row():
+                with gr.Column():
+                    input_2_1 = gr.Textbox(label='图片描述')
+                    input_2_2 = gr.Number(label='num_inference_steps', minimum=1, maximum=100, value=20)
+                    input_2_3 = gr.Dropdown(label='生成图片比例',
+                                            choices=['1:1', '1:2', '3:2', '3:4', '16:9', '9:16', ],
+                                            value='1:1')
+                    submit_btn_2 = gr.Button("提交", variant="primary")
+                with gr.Column():
+                    output_image_2 = gr.Image(label="生成的图片")
+
+            submit_btn_2.click(dosomething2, inputs=[input_2_1, input_2_2, input_2_3, ],
+                               outputs=output_image_2)
+
+    demo.launch(server_port=7861)
